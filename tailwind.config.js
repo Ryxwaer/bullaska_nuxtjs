64d1a1773ff5d37a9eaa52f6927eb42e4e730618
AdamPolicek <adam.policek@coalsoft.cz>
2024-01-28 00:11:50 +0100

    initial structure

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        'brown': '#c6a37d',
+      },
+      fontFamily: {
+        'work-sans': ['Work Sans', 'sans-serif'],
+        'satisfy': ['Satisfy', 'cursive'],
+      },
+      fontWeight: {
+        'work-sans-bold': '700',
+      },
+    },
+  },
+}
