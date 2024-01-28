78c5c7cb6a7a0024b397606c146a8ed52e89e801
AdamPolicek <adam.policek@coalsoft.cz>
2024-01-28 20:27:17 +0100

    swiper showcase of dogs added

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss", 'nuxt-icon', 'nuxt-swiper'],
+  modules: [
+    "@nuxtjs/tailwindcss",
+    'nuxt-icon',
+    'nuxt-swiper',
+    ['@storyblok/nuxt', { accessToken: 'YOUR_ACCESS_TOKEN', cacheProvider: 'memory' }],
+    '@nuxt/image',
+  ],
   tailwindcss: {
     cssPath: '~/assets/css/input.css'
   },
