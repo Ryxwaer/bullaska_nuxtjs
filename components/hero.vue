7f536460284f93cc6ff500bd0305c5f94ffdf083
AdamPolicek <adam.policek@coalsoft.cz>
2024-02-02 21:09:32 +0100

    compute better scaling for hero section background

--- components/hero.vue
+++ components/hero.vue
@@ -5,7 +5,8 @@
         <h1 class="text-white font-poppins-bold text-4xl sm:text-6xl bg-black w-[60%] min-h-[80px]">French Bulldog</h1>
         <NuxtImg src="/images/home/fci.png" sizes="102px" />
       </div>
-      <div class="absolute w-full h-full bg-no-repeat sm:bg-center bg-140% sm:bg-contain" :style="backgroundStyle"></div>
+      <div class="hidden md:block absolute w-full h-full bg-no-repeat sm:bg-center bg-140% sm:bg-contain" :style="backgroundStyle"></div>
+      <div class="md:hidden absolute w-full h-full bg-no-repeat sm:bg-center bg-140% sm:bg-contain" :style="backgroundStyleMobile"></div>
       <div class="bg-white text-center w-full mb-[182px]" >
         <div class="font-poppins-black sm:text-7xl text-5xl text-white" style="mix-blend-mode: difference;">
           <h1>Welcome to our kennel</h1>
@@ -29,6 +30,7 @@
 // Background position state
 const yOffset = ref(120);
 const yPos = ref(0 - yOffset.value);
+const yPosMobile = ref(0 - yOffset.value / 2);
 
 // Compute background style reactive property
 const backgroundStyle = computed(() => ({
@@ -36,10 +38,17 @@ const backgroundStyle = computed(() => ({
   backgroundPosition: `center ${yPos.value}px`
 }));
 
+const backgroundStyleMobile = computed(() => ({
+  backgroundImage: `url('/images/home/eyes.jpeg')`,
+  backgroundPosition: `center ${yPosMobile.value}px`
+}));
+
 // Method to update background position based on scroll
 const updateBackgroundPosition = () => {
   const speed = 0.48;
-  yPos.value = window.scrollY * speed - yOffset.value;
+  yPos.value = window.scrollY * speed;
+  yPosMobile.value = yPos.value - yOffset.value / 2;
+  yPos.value = yPos.value - yOffset.value;
 };
 
 // Listen to scroll event on mount
