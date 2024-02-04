02ff08d101db62c026cdba996d0faf2988814671
AdamPolicek <adam.policek@coalsoft.cz>
2024-02-04 13:33:21 +0100

    showcase edited for better immersion

--- components/showcase.vue
+++ components/showcase.vue
@@ -8,15 +8,17 @@
     </div>
     <div v-else class="grid items-center lg:grid-cols-2">
       <transition name="fade">
-        <div class="flex flex-col justify-between min-h-[260px] transition-opacity duration-500 ease-in-out"
+        <div class="flex flex-col justify-between min-h-[260px] transition-opacity duration-500 ease-in-out py-4 overflow-hidden"
              :class="{ 'opacity-0': !showDescription }">
           <div>
-            <div v-html="dogs[activeSlide].title" class="text-4xl min-h-[88px]"/>
-            <div v-html="dogs[activeSlide].excerpt"/>
+            <div v-html="dogs[activeSlide].title" class="text-4xl min-h-[88px]" @mouseenter="pauseSwiperAutoplay"
+                 @mouseleave="startSwiperAutoplay"/>
+            <div v-html="dogs[activeSlide].excerpt" class="text-lg"/>
           </div>
           <div class="text-right">
             <NuxtLink :to="`/blog/${dogs[activeSlide].slug}`"
-                      class="hover:text-black hover:bg-amber-500 hover:bg-opacity-40 text-gray-800 py-2 px-4 border border-black rounded shadow">
+                      class="hover:text-black hover:bg-amber-600 hover:bg-opacity-20 text-gray-800 py-2 px-4 border border-black rounded shadow"
+                      @mouseenter="pauseSwiperAutoplay" @mouseleave="startSwiperAutoplay">
               Čítať viac >>
             </NuxtLink>
           </div>
@@ -24,7 +26,6 @@
       </transition>
       <div>
         <Swiper
-            ref="swiper"
             class="swiper-cards w-72 h-72 lg:w-80 lg:h-80"
             :modules="[SwiperAutoplay, SwiperEffectCards]"
             :slides-per-view="1"
@@ -38,6 +39,7 @@
             @sliderMove="event => (showDescription = false)"
             @transitionStart="event => (showDescription = false)"
             @transitionEnd="event => (activeSlide = event.realIndex, showDescription = true)"
+            @swiper="onSwiper" @click="navigateToBlog(`/blog/${dogs[activeSlide].slug}`)"
         >
           <SwiperSlide v-for="(dog, idx) in dogs" :key="idx">
             <NuxtImg format="webp"
@@ -56,7 +58,24 @@ const {data: dogs, pending, error} = await useFetch(`/api/wp/dogs`);
 const width = ref(380);
 const activeSlide = ref(0);
 const showDescription = ref(true);
-const swiper = ref(null);
+
+const mySwiper = ref(null);
+
+function onSwiper(swiper) {
+  mySwiper.value = swiper;
+}
+
+function pauseSwiperAutoplay() {
+  mySwiper.value.autoplay.pause();
+}
+
+function startSwiperAutoplay() {
+  mySwiper.value.autoplay.resume()
+}
+
+const navigateToBlog = (route) => {
+  useRouter().push(route);
+};
 </script>
 
 <style scoped>
@@ -64,5 +83,6 @@ const swiper = ref(null);
   border-radius: 6px;
   border: 1px solid black;
   object-fit: cover;
+  cursor: pointer;
 }
 </style>
