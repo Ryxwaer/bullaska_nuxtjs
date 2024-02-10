5e1a9d64e9741870a3f1b552ad3a1c7ca2ce2f67
ryxwaer <ryxwaer@gmail.com>
2024-02-10 11:16:45 +0100

    pedigree responsive design

--- components/pedigree.vue
+++ components/pedigree.vue
@@ -1,109 +1,67 @@
 <template>
-  <div class="flex gap-2 md:gap-12 columns-3 bg-amber-600 bg-opacity-20 rounded-3xl h-[800px]">
-    <div class="flex flex-col gap-12 h-[80%] w-1/3 my-auto">
-      <div v-for="parent in data2.parents" :key="parent"
-           class="flex flex-1 flex-col justify-center bg-amber-500 rounded-3xl text-center hover:scale-x-125 hover:scale-y-125 transition duration-300 p-4 border">
-        <div class="mt-2">{{ parent }}</div>
+  <div class="overflow-x-auto bg-amber-600 bg-opacity-20 rounded-2xl p-4 scrollbar-hide" :class="gridClass"
+       @mouseenter="hovered = 1" @mouseleave="hovered = 0">
+    <div class="flex gap-2 md:gap-12 min-w-[800px] h-[800px] z-10">
+      <div class="flex flex-col gap-12 h-[80%] w-1/3 my-auto">
+        <div v-for="parent in data2.parents" :key="parent" @mouseenter="hovered = parent" @mouseleave="hovered = 1"
+             class="flex flex-1 flex-col justify-center bg-amber-500 rounded-2xl text-center transition duration-300 p-4 border"
+             :class="{ 'scale-125 z-10': hovered === parent, 'scale-95': hovered && hovered !== parent }">
+          <div class="rounded-2xl bg-white bg-opacity-20 h-3/4 w-3/4 mx-auto"></div>
+          <div class="mt-2">{{ parent }}</div>
+        </div>
       </div>
-    </div>
-    <div class="flex flex-col gap-4 h-full w-1/3">
-      <div v-for="parent in data2.grandparents" :key="parent"
-           class="flex flex-1 flex-col justify-center bg-amber-600 rounded-3xl text-center hover:scale-x-125 hover:scale-y-125 transition duration-300 p-4 border">
-        <div class="mt-2">{{ parent }}</div>
+      <div class="flex flex-col gap-4 h-full w-1/3">
+        <div v-for="parent in data2.grandparents" :key="parent" @mouseenter="hovered = parent" @mouseleave="hovered = 1"
+             class="flex flex-1 flex-col justify-center bg-amber-600 rounded-2xl text-center transition duration-300 p-4 border"
+             :class="{ 'scale-125 z-10 duration-100': hovered === parent, 'scale-95': hovered && hovered !== parent }">
+          <div class="rounded-2xl bg-white bg-opacity-20 h-3/4 w-3/4 mx-auto"></div>
+          <div class="mt-2">{{ parent }}</div>
+        </div>
       </div>
-    </div>
-    <div class="flex flex-col gap-2 h-full w-1/3">
-      <div v-for="parent in data2.greatGrandparents" :key="parent"
-           class="flex flex-1 flex-col justify-center bg-amber-800 rounded-3xl text-center hover:scale-x-125 hover:scale-y-125 transition duration-300 p-4 border">
-        <div class="mt-2">{{ parent }}</div>
+      <div class="flex flex-col gap-4 h-full w-1/3">
+        <div v-for="parent in data2.greatGrandparents" :key="parent" @mouseenter="hovered = parent" @mouseleave="hovered = 1"
+             class="flex flex-1 flex-col justify-center bg-amber-800 rounded-2xl text-center transition-all duration-300 p-4 border"
+             :class="{ 'scale-125 z-10 h-28 flex-auto transition-none': hovered === parent, 'scale-95 h-10': hovered && hovered !== parent }">
+          <div v-if="hovered === parent" class="rounded-2xl bg-white bg-opacity-20 h-3/4 w-3/4 mx-auto"></div>
+          <div class="mt-2">{{ parent }}</div>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import {computed, ref} from "vue";
+
 const data2 = {
   parents: ["mother", "father"],
   grandparents: ["grandmother1", "grandfather1", "grandmother2", "grandfather2"],
   greatGrandparents: ["greatGrandmother1", "greatGrandfather1", "greatGrandmother2", "greatGrandfather2", "greatGrandmother3", "greatGrandfather3", "greatGrandmother4", "greatGrandfather4"],
 };
 
-const data1 = [
-  {
-    name: "Mother",
-    image: "/images/home/eyes.jpeg",
-    parent: [
-      {
-        name: "Test11",
-        image: "/images/home/eyes.jpeg",
-        parent: [
-          {
-            name: "Test21",
-            image: "/images/home/eyes.jpeg",
-            parent: [],
-          },
-          {
-            name: "Test22",
-            image: "/images/home/eyes.jpeg",
-            parent: [],
-          }
-        ],
-      },
-      {
-        name: "Test12",
-        image: "/images/home/eyes.jpeg",
-        parent: [
-          {
-            name: "Test23",
-            image: "/images/home/eyes.jpeg",
-            parent: [],
-          },
-          {
-            name: "Test24",
-            image: "/images/home/eyes.jpeg",
-            parent: [],
-          }
-        ],
-      }
-    ],
-  },
-  {
-    name: "Father",
-    image: "/images/home/eyes.jpeg",
-    parent: [
-      {
-        name: "Test13",
-        image: "/images/home/eyes.jpeg",
-        parent: [
-          {
-            name: "Test25",
-            image: "/images/home/eyes.jpeg",
-            parent: [],
-          },
-          {
-            name: "Test26",
-            image: "/images/home/eyes.jpeg",
-            parent: [],
-          }
-        ],
-      },
-      {
-        name: "Test14",
-        image: "/images/home/eyes.jpeg",
-        parent: [
-          {
-            name: "Test27",
-            image: "/images/home/eyes.jpeg",
-            parent: [],
-          },
-          {
-            name: "Test28",
-            image: "/images/home/eyes.jpeg",
-            parent: [],
-          }
-        ],
-      }
-    ],
-  },
-];
+const hovered: any = ref(0);
+
+// Adjust the grid dynamically based on the hovered cell
+const gridClass = computed(() => {
+  if (!hovered.value) return '';
+  // Example class adjustments, could be more sophisticated based on exact needs
+  return 'gap-1';
+});
 </script>
+
+<style scoped>
+/* Custom class to hide scrollbar */
+.scrollbar-hide {
+  /* For Webkit (Safari, Chrome) */
+  -webkit-overflow-scrolling: touch;
+  &::-webkit-scrollbar {
+    display: none;
+  }
+
+  /* For IE, Edge */
+  -ms-overflow-style: none;
+
+  /* For Firefox */
+  scrollbar-width: none;
+}
+</style>
